963dd2ff04e9d0839bbb052ec09aac14519cbc8f
Toby Donaldson <tjd@sfu.ca>
2023-07-03 11:16:40 -0700

    updated

--- assignments/a4/a4_sort_implementations.h
+++ assignments/a4/a4_sort_implementations.h
@@ -1,5 +1,29 @@
 // a4_sort_implementations.h
 
+/////////////////////////////////////////////////////////////////////////
+//
+// Student Info
+// ------------
+//
+// Name : <put your full name here!>
+// St.# : <put your full SFU student number here>
+// Email: <put your SFU email address here>
+//
+//
+// Statement of Originality
+// ------------------------
+//
+// All the code and comments below are my own original work. For any non-
+// original work, I have provided citations in the comments with enough
+// detail so that someone can see the exact source and extent of the
+// borrowed work.
+//
+// In addition, I have not shared this work with anyone else, and I have
+// not seen solutions from other students, tutors, websites, books,
+// etc.
+//
+/////////////////////////////////////////////////////////////////////////
+
 #pragma once
 
 #include "a4_base.h"
